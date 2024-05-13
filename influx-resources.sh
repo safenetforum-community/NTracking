eefa667480bba27136bd2db8370006f7bc19d7b6
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-05-12 21:08:59 -0400

    Update influx-resources.sh

--- influx-resources.sh
+++ influx-resources.sh
@@ -2,6 +2,11 @@
 
 #11/05/2024 19:13
 
+# if cpu over 90% exit monitoring script
+cpu=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) ; }' \
+<(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))
+if [ 1 -eq "$(echo "$cpu > 90.0" | bc)" ]; then exit 0; fi
+
 # Environment setup
 export PATH=$PATH:$HOME/.local/bin
 base_dir="/var/safenode-manager/services"
