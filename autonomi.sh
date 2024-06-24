cbf91b4f7a83b4cdc65a86c3e97b8265711a906d
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-06-24 16:21:42 +0100

    Update autonomi.sh

--- autonomi.sh
+++ autonomi.sh
@@ -64,7 +64,7 @@ fi
 
 #install latest infux resources script from github
 sudo rm /usr/bin/influx-resources.sh* && sudo wget -P /usr/bin  https://raw.githubusercontent.com/safenetforum-community/NTracking/main/influx-resources.sh && sudo chmod u+x /usr/bin/influx-resources.sh
-echo "*/15 * * * * $USER /usr/bin/mkdir -p /tmp/influx-resources && /bin/bash /usr/bin/influx-resources.sh > /tmp/influx-resources/influx-resources" | sudo tee /etc/cron.d/influx_resources
+echo "0 * * * * $USER /usr/bin/mkdir -p /tmp/influx-resources && /bin/bash /usr/bin/influx-resources.sh > /tmp/influx-resources/influx-resources" | sudo tee /etc/cron.d/influx_resources
 
 #install latest load balancing script
 sudo rm -f /usr/bin/node-balance.sh && sudo wget -P /usr/bin/  https://raw.githubusercontent.com/safenetforum-community/NTracking/main/node-balance.sh && sudo chmod u+x /usr/bin/node-balance.sh
