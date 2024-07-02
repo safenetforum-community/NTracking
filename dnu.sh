5c6ad77c531960b538e3f32518b703238c28bee7
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-07-02 12:20:39 +0100

    Update dnu.sh

--- dnu.sh
+++ dnu.sh
@@ -24,7 +24,7 @@ button=black,white
 
 ############################################## select test net action
 
-SELECTION=$(whiptail --title "Autonomi Network Beta 1.4 " --radiolist \
+SELECTION=$(whiptail --title "Autonomi Network Beta 1.5 " --radiolist \
 "Testnet Actions                              " 20 70 10 \
 "1" "Install & Start Nodes " OFF \
 "2" "Upgrade Client to Latest" OFF \
