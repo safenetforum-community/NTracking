4d17a4994da7703f8d66d1124380c0837b93aba6
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-08-27 19:58:34 +0100

    Update NTracking.sh

--- NTracking.sh
+++ NTracking.sh
@@ -594,7 +594,7 @@ sudo tee /etc/telegraf/telegraf.conf 2>&1 > /dev/null <<EOF
   # ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
 
 [[inputs.net]]
-  interfaces = ["enp*", "eno*", "eth*", "ib*", "wl*"]
+  interfaces = ["enp*", "ens*", "eno*", "eth*", "ib*", "wl*"]
 
 [[inputs.tail]]
   files = ["/tmp/influx-resources/influx-resources"]
