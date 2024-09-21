1b78bcc3954b29c37de39876c3fa67264f1a2fd5
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-09-21 20:51:39 +0100

    Update NTracking.sh

--- NTracking.sh
+++ NTracking.sh
@@ -581,7 +581,7 @@ sudo tee /etc/telegraf/telegraf.conf 2>&1 > /dev/null <<EOF
 [[inputs.system]]
   # no configuration
 
-  # Read metrics about temperature
+# Read metrics about temperature
 [[inputs.temp]]
 
 # Get the number of processes and group them by status
