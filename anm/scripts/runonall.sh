5843c5263db8037531efb111ca4810c693023d8d
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-10-09 22:10:29 +0100

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -47,8 +47,8 @@ sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercont
 #sed -i "s/^\\(DesiredLoadAverage=\\).*/\\DesiredLoadAverage=18.0/" /var/safenode-manager/config
 
 
-sed -i "s/^\\(MemLessThan=\\).*/\\MemLessThan=75/" /var/safenode-manager/config
-sed -i "s/^\\(MemRemove=\\).*/\\MemRemove=95/" /var/safenode-manager/config
+#sed -i "s/^\\(MemLessThan=\\).*/\\MemLessThan=75/" /var/safenode-manager/config
+#sed -i "s/^\\(MemRemove=\\).*/\\MemRemove=95/" /var/safenode-manager/config
 
 
-#rm -rf $HOME/.local/share/safe/
+rm -rf $HOME/.local/share/safe/
