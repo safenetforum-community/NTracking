1291cccaf96c1e7ba858ae41e69c316557a92ba8
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-10-26 07:42:45 +0100

    Update influx-resources.sh

--- influx-resources.sh
+++ influx-resources.sh
@@ -44,7 +44,7 @@ for ((i = 1; i <= $NumberOfNodes; i++)); do
 
     if [[ -n "$node_details" ]]; then
         total_nodes_running=$(($total_nodes_running + 1))
-        status="TRUE"
+        status="Running"
         mem_used=$(echo "$node_details" | grep sn_networking_process_memory_used_mb | awk 'NR==3 {print $2}')
         cpu_usage=$(echo "$node_details" | grep sn_networking_process_cpu_usage_percentage | awk 'NR==3 {print $2}')
         records=$(echo "$node_details" | grep sn_networking_records_stored | awk 'NR==3 {print $2}')
@@ -56,6 +56,11 @@ for ((i = 1; i <= $NumberOfNodes; i++)); do
         store_cost=$(echo "$node_details" | grep sn_networking_store_cost | awk 'NR==3 {print $2}')
         gets=$(echo "$node_details" | grep libp2p_kad_query_result_get_record_ok_total | awk '{print $2}')
         puts=$(echo "$node_details" | grep sn_node_put_record_ok_total | awk '{print $2}' | paste -sd+ | bc)
+        live_time=$(echo "$node_details" | grep sn_networking_live_time | awk 'NR==3 {print $2}')
+        rel_records=$(echo "$node_details" | grep sn_networking_relevant_records | awk 'NR==3 {print $2}')
+        
+
+
         if [[ -z "$puts" ]]; then
             puts=0
         fi
@@ -75,17 +80,20 @@ for ((i = 1; i <= $NumberOfNodes; i++)); do
 
     else
         total_nodes_killed=$(($total_nodes_killed + 1))
-        status="FALSE"
+        status="Stopped"
         mem_used=0
         cpu_usage=0
         records=0
         network_size=0
         shunned_count=0
+        bad_peers=0
         rewards_balance=0
         connected_peers=0
         store_cost=0
         gets=0
         puts=0
+        live_time=0
+        rel_records=0
 
         if [[ -f "/var/safenode-manager/NodeDetails" ]]; then
             # for anm
@@ -105,11 +113,12 @@ for ((i = 1; i <= $NumberOfNodes; i++)); do
     fi
 
     # Format for InfluxDB
-    node_details_str[$i]="nodes,id=$node_name PeerId=$PeerId,status=$status,records="$records"i,connected_peers="$connected_peers"i,rewards=$rewards_balance,store_cost="$store_cost"i,cpu="$cpu_usage"i,mem="$mem_used"i,puts="$puts"i,gets="$gets"i,version=$NodeVersion,networ_size="$network_size"i,shunned_count="$shunned_count"i,bad_peers="$bad_peers"i $influx_time"
+    node_details_str[$i]="nodes,id=$node_name PeerId=$PeerId,status=$status,records="$records"i,connected_peers="$connected_peers"i,rewards=$rewards_balance,store_cost="$store_cost"i,cpu="$cpu_usage"i,mem="$mem_used"i,puts="$puts"i,gets="$gets"i,version=$NodeVersion,networ_size="$network_size"i,shunned_count="$shunned_count"i,bad_peers="$bad_peers"i,live_time="$live_time"i,rel_records="$rel_records"i $influx_time"
     #sleep to slow script down to spread out cpu spike
 
-    rewards_balance=$(echo "scale=10; $rewards_balance / 1000000000" | bc)
-    total_rewards_balance=$(echo "scale=10; $total_rewards_balance + $rewards_balance" | bc -l)
+    #rewards_balance=$(echo "scale=10; $rewards_balance / 1000000000" | bc)
+    #total_rewards_balance=$(echo "scale=10; $total_rewards_balance + $rewards_balance" | bc -l)
+    total_rewards_balance=$(echo "$total_rewards_balance + $rewards_balance" | bc -l)
     total_network_size=$(($total_network_size + $network_size))
 
 done
@@ -118,6 +127,8 @@ network_size=$(echo "$total_network_size / $total_nodes_running" | bc)
 
 # Latency
 latency=$(ping -c 4 8.8.8.8 | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
+
+if [[ $time_min == 0 ]] || [[ $time_min == 20 ]] || [[ $time_min == 40 ]]; then
 
 ##############################################################################################
 # coin gecko gets upset with to many requests this atempts to get the exchange every 15 min
@@ -130,8 +141,9 @@ exchange_rate_usd=$(awk -F'[:,]' '{print $7}' <<<$coingecko)
 market_cap_usd=$(awk -F'[:}]' '{print $6}' <<<$coingecko)
 
 # calculate earnings in usd & gbp
-earnings_gbp=$(echo $total_rewards_balance*$exchange_rate_gbp | bc)
-earnings_usd=$(echo $total_rewards_balance*$exchange_rate_usd | bc)
+earnings_gbp=$(echo $total_rewards_balance*$exchange_rate_gbp/1000000000000000000 | bc)
+earnings_usd=$(echo $total_rewards_balance*$exchange_rate_usd/1000000000000000000 | bc)
+fi
 
 # calculate total storage of the node services folder
 total_disk=$(echo "scale=0;("$(du -s "$base_dir" | cut -f1)")/1024" | bc)
