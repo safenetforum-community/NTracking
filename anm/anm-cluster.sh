8669b0dc9cf9572a116666716d6816d2ec11e8b5
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-10-30 09:16:44 +0000

    Update anm-cluster.sh

--- anm/anm-cluster.sh
+++ anm/anm-cluster.sh
@@ -67,9 +67,9 @@ elif [[ "$SELECTION" == "2" ]]; then
         exit 0
     fi
 
-    #NodeCountChange='sleep 120 && sed -i "s/^\\(NodeCap=\\).*/\\NodeCap='$NodeCount'/" /var/safenode-manager/config '
+    NodeCountChange='sleep 120 && sed -i "s/^\\(NodeCap=\\).*/\\NodeCap='$NodeCount'/" /var/safenode-manager/config '
     #NodeCountChange='sleep 120 && sed -i "s/^\\(CpuRemove=\\).*/\\CpuRemove='$NodeCount'/" /var/safenode-manager/config '
-    NodeCountChange='sleep 120 && sed -i "s/^\\(MaxLoadAverageAllowed=\\).*/\\MaxLoadAverageAllowed='$NodeCount'/" /var/safenode-manager/config '
+    #NodeCountChange='sleep 120 && sed -i "s/^\\(MaxLoadAverageAllowed=\\).*/\\MaxLoadAverageAllowed='$NodeCount'/" /var/safenode-manager/config '
 
     for machine in $machines; do
         ssh -t $machine ''$NodeCountChange'' >/dev/null 2>&1 &
