47bbc6a12ae6e0bec94807c2468dd56dd7613c1a
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-11-07 11:40:20 +0000

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -64,5 +64,8 @@
 
 #sudo sed -i 's/PGRADEHOUR=18/UpgradeHour=19/g' /var/safenode-manager/config
 
+sudo apt update
+
 $HOME/.cargo/bin/rustup target install x86_64-unknown-linux-musl
-sudo apt install musl musl-dev 
+sudo apt install musl musl-dev
+sudo apt install musl-tools
