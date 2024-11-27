69302a76ff60210d06259b0059833a5eeccb72d4
Your Name <you@example.com>
2024-11-27 12:00:54 +0000

    test

--- anm/scripts/anms.sh
+++ anm/scripts/anms.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-vtrack="1.3"
+vtrack="1.4"
 
 # sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
 
