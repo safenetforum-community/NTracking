b074fd7f1b225c2744a5e8b45941dad98cb17c86
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-11-27 06:23:19 +0000

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -9,6 +9,10 @@
 #rm /var/safenode-manager/config
 #fi
 
+sudo apt install linux-tools-generic -y
+# cpupower -c all frequency-info | grep gov ; cpupower frequency-info
+sudo cpupower frequency-set --governor performance
+
 #sudo swapoff -a
 
 #sudo ufw disable
@@ -22,7 +26,7 @@
 
 #touch $HOME/runonallsystems
 
-sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
+#sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
 #sudo rm -f /usr/bin/influx-resources.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/influx-resources.sh && sudo chmod u+x /usr/bin/influx-resources.sh
 
 #make swap file
