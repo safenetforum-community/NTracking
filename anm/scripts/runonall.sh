a0265c447e5e6f5204d8e8c6ebb1e0df665a1ff2
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-12-21 12:40:24 +0000

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -27,7 +27,7 @@
 
 #touch $HOME/runonallsystems
 
-sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
+#sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
 #sudo rm -f /usr/bin/influx-resources.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/influx-resources.sh && sudo chmod u+x /usr/bin/influx-resources.sh
 
 #make swap file
@@ -87,3 +87,7 @@ sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercont
 #sudo rm /usr/local/bin/antup
 
 #curl -sSL https://raw.githubusercontent.com/maidsafe/antup/main/install.sh | sudo bash
+
+rm /var/antctl/simplelog
+rm /var/antctl/log
+rm /var/antctl/wallet-log
