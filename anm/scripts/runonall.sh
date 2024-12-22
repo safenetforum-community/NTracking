abe2550b3debf82e30c67ec22f9c81ecebe6e7b0
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2024-12-22 22:19:47 +0000

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -51,10 +51,8 @@
 #sed -i "s/^\\(UpgradeHour=\\).*/\\UpgradeHour=16/" /var/antctl/config
 #sed -i "s/^\\(NodeCap=\\).*/\\NodeCap=100/" /var/antctl/config
 
-#sed -i "s/^\\(MaxLoadAverageAllowed=\\).*/\\MaxLoadAverageAllowed=24.0/" /var/antctl/config
-#sed -i "s/^\\(DesiredLoadAverage=\\).*/\\DesiredLoadAverage=18.0/" /var/antctl/config
-
-sed -i "s/^\\(MemLessThan=80\\).*/\\CpuLessThan=80/" /var/antctl/config
+sed -i "s/^\\(MaxLoadAverageAllowed=\\).*/\\MaxLoadAverageAllowed=30.0/" /var/antctl/config
+sed -i "s/^\\(DesiredLoadAverage=\\).*/\\DesiredLoadAverage=24.0/" /var/antctl/config
 
 
 #sed -i "s/^\\(MemLessThan=\\).*/\\MemLessThan=90/" /var/antctl/config
