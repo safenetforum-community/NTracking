1d3e683cf26e6db1dc5228790a0915432c7ec17f
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2025-01-05 12:23:29 +0000

    Update anms.sh

--- anm/scripts/anms.sh
+++ anm/scripts/anms.sh
@@ -474,7 +474,7 @@ ShunnGun() {
 
 LoadTrimmer() {
 
-    if (($("$time_min") == 2)) || (($("$time_min") == 17)) || (($("$time_min") == 32)) || (($("$time_min") == 47)); then
+    if [[ $time_min == 2 ]] || [[ $time_min == 17 ]] || [[ $time_min == 32 ]] || [[ $time_min == 47 ]]; then
 
         if (($(echo "$Upgrade != 0" | bc))); then
             echo "node replacement not allowed during upgrade" && echo
