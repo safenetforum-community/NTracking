73dde02ccf4fefb815e8b672e5e5fa07c133f70f
aautonomicc <25412853+aautonomicc@users.noreply.github.com>
2025-02-11 18:10:22 +0000

    Update runonall.sh

--- anm/scripts/runonall.sh
+++ anm/scripts/runonall.sh
@@ -108,5 +108,7 @@
 #sleep 5
 #sudo rm -f /usr/bin/anms.sh* && sudo wget -P /usr/bin https://raw.githubusercontent.com/safenetforum-community/NTracking/main/anm/scripts/anms.sh && sudo chmod u+x /usr/bin/anms.sh
 
-rm /var/antctl/block
-rm /var/antctl/config
+#rm /var/antctl/block
+#rm /var/antctl/config
+
+rf $HOME/.local/share/autonomi/anm-control.sh
